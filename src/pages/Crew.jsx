0f0d88fc626f2722c252fbe83a915059c14a1865
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-08 20:58:02 +0500

    configuring routes

---
+++ src/pages/Crew.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Crew = () => {
+  return (
+    <div className='text-balance'>Crew</div>
+  )
+}
+
+export default Crew
---
+++ src/pages/Destination.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Destination = () => {
+  return (
+    <div className='text-balance'>Destination</div>
+  )
+}
+
+export default Destination
