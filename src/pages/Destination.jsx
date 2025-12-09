b86cfcc27aaa99cee3e682a21b3cc84411d0676f
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-09 23:45:05 +0500

    destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import { useState } from "react"
+import data from "/src/data/data.json"
 
 const Destination = () => {
+  const [currDestination, setCurrDestination] = useState('moon');
+  const destinations = data.destinations;
   return (
-    <div className='bg-[url(/src/assets/home/background-home-mobile.jpg)] sm:bg-[url(/src/assets/home/background-home-tablet.jpg)] lg:bg-[url(/src/assets/home/background-home-desktop.jpg)] bg-fixed bg-no-repeat bg-cover min-h-screen w-full'
+    <div className='bg-[url(/src/assets/destination/background-destination-mobile.jpg)] sm:bg-[url(/src/assets/destination/background-destination-tablet.jpg)] lg:bg-[url(/src/assets/destination/background-destination-desktop.jpg)] bg-fixed bg-no-repeat bg-cover min-h-screen w-full'
       aria-label='Space travel destinations' >
       <div className='container'>
-      <h1 className='text-white'><span className='text-gray-500 font-bold'>01</span> Pick your destination</h1>
-      <main>
-        <section></section>
-      </main>
-    </div>
+        <h1 className='text-white'><span className='text-gray-500 font-bold'>01</span> Pick your destination</h1>
+        <main>
+          <section className="flex justify-between items-baseline">
+            <div>
+              {
+                destinations.map((destination, index) => (
+                  <img key={index} src={destination.images.png} />
+                ))
+              }
+            </div>
+            <div>
+              <nav>
+                <li onClick={()=>setCurrDestination("Moon")}>Moon</li>
+                <li onClick={()=>setCurrDestination("Mars")}>Mars</li>
+                <li onClick={()=>setCurrDestination("Europa")}>Europa</li>
+                <li  onClick={()=>setCurrDestination("Titan")}>Titan</li>
+              </nav>
+              {
+                console.log(currDestination)
+              
+              }
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   )
 }
