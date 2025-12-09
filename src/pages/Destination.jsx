659b20a968678514283896e43fdd6af8996e0ac4
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-09 23:07:51 +0500

    styles update

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Destination = () => {
   return (
-    <div className='text-balance'>Destination</div>
+    <div className='bg-[url(/src/assets/home/background-home-mobile.jpg)] sm:bg-[url(/src/assets/home/background-home-tablet.jpg)] lg:bg-[url(/src/assets/home/background-home-desktop.jpg)] bg-fixed bg-no-repeat bg-cover min-h-screen w-full'
+      aria-label='Space travel destinations' >
+      <div className='container'>
+      <h1 className='text-white'><span className='text-gray-500 font-bold'>01</span> Pick your destination</h1>
+      <main>
+        <section></section>
+      </main>
+    </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,10 @@ const Home = () => {
       <main className="container flex justify-center items-center lg:items-baseline-last max-w-1000">
         <section className="flex flex-col lg:flex-row lg:justify-between items-center text-lightblue w-full">
           <div className="mb-100 text-center lg:text-left">
-            <h1 className="font-barlow-condensed text-md md:text-lg uppercase tracking-[2px] sm:tracking-[4px] font-light mb-20">
+            <h1 className="mb-20">
               So, you want to travel to
             </h1>
-            <h2 className="text-white text-3xl md:text-4xl  font-bellefair uppercase ">
+            <h2 className="text-3xl md:text-4xl">
               Space
             </h2>
             <p className="mt-30 text-sm leading-relaxed font-barlow max-w-540">
