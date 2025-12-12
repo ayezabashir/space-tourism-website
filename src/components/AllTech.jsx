42017ab393cba92b2c140e46e65468c7bde21bcd
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-12 23:09:36 +0500

    tech page update

--- src/components/AllTech.jsx
+++ src/components/AllTech.jsx
@@ -5,27 +5,35 @@ import Content from "./Content";
 const AllTech = () => {
     const [currTech, setCurrTech] = useState(0);
     const techs = data.technology;
-    const tech = techs.find((tech)=>tech.id===currTech);
+    const tech = techs.find((tech) => tech.id === currTech);
     return (
-        <section className="flex flex-col xl:flex-row xl:h-600 justify-between gap-20 items-center">
-            <div>
-                <div className="flex md:flex-col gap-20">
+        <section className="flex flex-col-reverse xl:flex-row xl:h-600 justify-between gap-50 xl:gap-20 items-center">
+            <div className="xl:w-3/5 flex flex-col items-center xl:items-start xl:flex-row gap-30">
+                <div className="flex xl:flex-col gap-20">
                     {
-                        techs.map((item, index)=>
-                            <p 
-                                key={index} 
-                                className="border border-white flex justify-center items-center h-50 w-50 rounded-full"
-                                onClick={()=>setCurrTech(item.id)}
+                        techs.map((item, index) =>
+                            <p
+                                key={index}
+                                className={`border border-white flex justify-center items-center h-50 w-50 rounded-full
+                                    ${currTech === item.id ? "bg-white text-darkblue" : "bg-transparent"}
+                                    `}
+                                onClick={() => setCurrTech(item.id)}
                             >
-                                {item.id+1}
+                                {item.id + 1}
                             </p>
                         )
                     }
                 </div>
-                <Content mainhead="The Terminolofy" subhead={tech.name} desc={tech.description}  />
+                <Content mainhead="The Terminology" subhead={tech.name} desc={tech.description} />
             </div>
-            <div>
-
+            <div className="w-full xl:w-2/5">
+                <picture>
+                    <source
+                        media="(max-width: 1279px)"
+                        srcSet={tech.images.landscape}
+                    />
+                    <img className="xl:absolute w-full xl:w-min right-0 top-0 transform xl:translate-y-[30%]" src={tech.images.portrait} alt="" />
+                </picture>
             </div>
         </section>
     )
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,9 @@
 const Content = ({mainhead, subhead, desc}) => {
     return (
         <div className="mb-50 text-center xl:text-start">
-            <h1 className="font-bellefair text-[#8d8f93] text-md sm:text-xl ">{mainhead}</h1>
+            <h1 className="font-bellefair text-[#8d8f93] text-md sm:text-xl mb-5">{mainhead}</h1>
             <h2 className="font-bellefair text-xl sm:text-2xl">{subhead}</h2>
-            <p className="leading-relaxed my-20 text-lightblue">{desc}</p>
+            <p className="leading-relaxed text-sm md:text-md my-20 text-lightblue">{desc}</p>
         </div>
     )
 }
