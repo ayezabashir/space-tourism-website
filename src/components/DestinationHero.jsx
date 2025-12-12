354a93ebbcdbc6a2c65af644ba4822a620f21446
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-12 18:31:00 +0500

    destinations page

--- src/components/DestinationHero.jsx
+++ src/components/DestinationHero.jsx
@@ -1,12 +1,12 @@
+import HeroComponent from "./HeroComponent"
 import AllDestinations from "./AllDestinations"
 const DestinationHero = () => {
   return (
-    <main className='container'>
-      <div className="max-w-1100 mx-auto my-100">
-        <h1 className='text-white mb-100'><span className='text-gray-500 font-bold'>01</span> Pick your destination</h1>
-        <AllDestinations />
-      </div>
-    </main>
+     <>
+      <HeroComponent>
+         <AllDestinations />
+      </HeroComponent>
+     </>
   )
 }
 
--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -1,6 +1,12 @@
-
-const HeroComponent = () => {
-   
+const HeroComponent = ({children}) => {
+    return <main className='container'>
+        <div className="max-w-1100 mx-auto my-100">
+            <h1 className='text-white mb-100'><span className='text-gray-500 font-bold'>01</span> Pick your destination</h1>
+            {
+                children
+            }
+        </div>
+    </main>
 }
 
 export default HeroComponent
