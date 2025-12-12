9a30e63493d6cc9becfd52f5d1b4966564ba9e5c
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-12 18:14:59 +0500

    styles update

--- src/components/AllDestinations.jsx
+++ src/components/AllDestinations.jsx
@@ -11,14 +11,14 @@ const AllDestinations = () => {
             <div className="p-20 flex justify-start">
                 <img className="w-200 sm:w-400 lg:w-500" src={selectedDestination.images.png} alt={selectedDestination.name} />
             </div>
-            <div className="mt-50 lg:mt-0 text-center lg:text-start max-w-460">
-                <nav className="flex justify-between px-20 items-center mb-30 gap-10 font-barlow font-light text-lightblue">
+            <div className="mt-50 lg:mt-0 text-center lg:text-start max-w-500">
+                <nav className="flex justify-between px-30 items-center mb-30 gap-10 font-barlow font-light text-lightblue">
                     <li className={`list-none mx-5 cursor-pointer ${currDestination === "Moon" ? "underline underline-offset-8 text-white" : "text-lightblue"}`} onClick={() => setCurrDestination("Moon")}>Moon</li>
                     <li className={`list-none mx-5 cursor-pointer ${currDestination === "Mars" ? "underline underline-offset-8 text-white" : "text-lightblue"}`} onClick={() => setCurrDestination("Mars")}>Mars</li>
                     <li className={`list-none mx-5 cursor-pointer ${currDestination === "Europa" ? "underline underline-offset-8 text-white" : "text-lightblue"}`} onClick={() => setCurrDestination("Europa")}>Europa</li>
                     <li className={`list-none mx-5 cursor-pointer ${currDestination === "Titan" ? "underline underline-offset-8 text-white" : "text-lightblue"}`} onClick={() => setCurrDestination("Titan")}>Titan</li>
                 </nav>
-                <div>
+                <div className="lg:px-30">
                     <h2 className="text-3xl">{selectedDestination.name}</h2>
                     <p className="text-sm text-lightblue font-light leading-relaxed my-20 max-w-430">{selectedDestination.description}</p>
                     <div className="my-40 flex flex-col gap-20 sm:justify-between lg:justify-start lg:gap-50 sm:flex-row pt-40 border-t border-gray-400">
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
 const Crew = () => {
   return (
-    <div className='text-balance'>Crew</div>
+   <div className='bg-[url(/src/assets/crew/background-crew-mobile.jpg)] sm:bg-[url(/src/assets/crew/background-crew-tablet.jpg)] lg:bg-[url(/src/assets/crew/background-crew-desktop.jpg)] bg-fixed bg-no-repeat bg-cover min-h-screen w-full'
+      aria-label='Space travel Crew' >
+       hi
+    </div>
   )
 }
 
