c7f120a88db517c7840ccb171f2431473e4a5166
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-13 15:37:23 +0500

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import logo from "/src/assets/shared/logo.svg"
+// import menuicon from "/src/assets/icon-hamburger.svg";
+// import closeicon from "/src/assets/icon-close.svg";
 
 const Header = () => {
-  return (
-    <header className='container'>
-
-    </header>
-  )
+    const nav = useNavigate();
+    return (
+        <header className='container'>
+            <nav>
+                <picture>
+                    <img src={logo} alt="" />
+                </picture>
+                <div>
+                    <a onClick={() => nav('/')}>00 Home</a>
+                    <a onClick={() => nav("/destination")}>01 Destination</a>
+                    <a onClick={() => nav("/crew")}>02 Crew</a>
+                    <a onClick={() => nav("/technology")}>03 Technology</a>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
