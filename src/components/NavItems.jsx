c2455dce0210ccee1e88c72cc4725f9d806b118f
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-13 20:08:54 +0500

    navitems added

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,31 +1,41 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const NavItems = () => {
-    const nav = useNavigate();
-    const currLocation = useLocation();
-    const currPage = currLocation.pathname
+    const baseClass = "cursor-pointer transition-all ease-in-out underline-offset-8 hover:underline";
     return (
         <>
-            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
-                            ${currPage==="/" ? "underline" : ""} `}
-                onClick={() => nav('/')}>
-                <span className="font-medium">00</span>  Home
-            </a>
-            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
-                            ${currPage==="/destination" ? "underline" : ""} `}
-                onClick={() => nav("/destination")}>
-                <span className="font-medium">01</span>  Destination
-            </a>
-            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
-                            ${currPage==="/crew" ? "underline" : ""} `}
-                onClick={() => nav("/crew")}>
-                <span className="font-medium">02</span>  Crew
-            </a>
-            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
-                            ${currPage==="/technology" ? "underline" : ""} `}
-                onClick={() => nav("/technology")}>
-                <span className="font-medium">03</span>  Technology
-            </a>
+            <NavLink
+                to="/"
+                className={({ isActive }) =>
+                    `${baseClass} ${isActive ? "underline" : ""}`
+                }
+            >
+                <span className='font-medium'>00</span> Home
+            </NavLink>
+            <NavLink
+                to="/destination"
+                className={({isActive})=>
+                    `${baseClass} ${isActive ? "underline" : ""}`
+                }  
+            >
+                <span className='font-medium'>01</span> Destination
+            </NavLink>
+            <NavLink
+                to="/crew"
+                className={({isActive})=>
+                    `${baseClass} ${isActive ? "underline":""}`
+                }
+            >
+                <span className='font-medium'>02</span> Crew
+            </NavLink>
+            <NavLink
+                to="/technology"
+                className={({isActive})=>
+                    `${baseClass} ${isActive ? "underline":""}`
+                }
+            >
+                <span className='font-medium'>03</span> Technology
+            </NavLink>
         </>
     )
 }
