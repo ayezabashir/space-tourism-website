4558faa7b8c5d19ae46ebe888568ac50f95b5981
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-13 20:00:37 +0500

    navitems updated

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,13 +1,31 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const NavItems = () => {
     const nav = useNavigate();
+    const currLocation = useLocation();
+    const currPage = currLocation.pathname
     return (
         <>
-            <a className="hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer" onClick={() => nav('/')}><span className="font-medium">00</span>  Home</a>
-            <a className="hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer" onClick={() => nav("/destination")}><span className="font-medium">01</span>  Destination</a>
-            <a className="hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer" onClick={() => nav("/crew")}><span className="font-medium">02</span>  Crew</a>
-            <a className="hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer" onClick={() => nav("/technology")}><span className="font-medium">03</span>  Technology</a>
+            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
+                            ${currPage==="/" ? "underline" : ""} `}
+                onClick={() => nav('/')}>
+                <span className="font-medium">00</span>  Home
+            </a>
+            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
+                            ${currPage==="/destination" ? "underline" : ""} `}
+                onClick={() => nav("/destination")}>
+                <span className="font-medium">01</span>  Destination
+            </a>
+            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
+                            ${currPage==="/crew" ? "underline" : ""} `}
+                onClick={() => nav("/crew")}>
+                <span className="font-medium">02</span>  Crew
+            </a>
+            <a className={`hover:underline underline-offset-8 transition-all ease-in-out delay-75 cursor-pointer
+                            ${currPage==="/technology" ? "underline" : ""} `}
+                onClick={() => nav("/technology")}>
+                <span className="font-medium">03</span>  Technology
+            </a>
         </>
     )
 }
